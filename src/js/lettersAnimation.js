5263541dc7b3dbe028a75bd4d25082ef96a9f615
VladEvst1824OP <43822322+VladEvst1824OP@users.noreply.github.com>
2019-09-29 19:25:41 +0300

    updated hpme page

---
+++ src/js/lettersAnimation.js
@@ -0,0 +1,4 @@
+// let letters = document.querySelectorAll('#text path');
+// [...letters].forEach(item => console.log(item.getTotalLength()))
+let ul = document.querySelector('.options');
+window.onload = () => ul.classList.add('appear');
