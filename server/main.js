d7c61d4473f3620dd72ba2075862635d67766e3c
VladEvst1824OP <43822322+VladEvst1824OP@users.noreply.github.com>
2019-09-30 21:38:38 +0300

    update

--- server/main.js
+++ server/main.js
@@ -2,17 +2,14 @@ const express = require('express');
 const app = express();
 const compression = require('compression');
 const PORT = process.env.PORT || 8080;
-app.use(compression({ filter: shouldCompress }));
 function shouldCompress(req, res) {
   if (req.headers['x-no-compression']) {
     return false;
   }
   return compression.filter(req, res);
 }
+app.use(compression({ filter: shouldCompress }));
 app.use(express.static('public'));
-app.get('/users', function(req, res) {
-  res.send('users end point');
-});
 app.listen(PORT, () => {
   console.log(`Connected to port ${PORT}`);
 });
